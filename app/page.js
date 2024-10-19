b101261a65f0f115bcc935d81d32f45c2e172457
yurii bilous <yurii.b@elogic.com.ua>
2024-10-19 11:42:52 +0200

    fetch data and render posts on homepage

--- app/page.js
+++ app/page.js
@@ -1,8 +1,26 @@
+import Link from "next/link";
 
-export default function Home() {
+async function fetchData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const result = await res.json();
+  return result;
+}
+
+export default async function Home() {
+
+  const res = await fetchData();
+  
+
   return (
     <div>
-      Main page
+      <h1>Home page</h1>
+      {res.map((post) => (
+        <div key={post.id} className="post">
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+          <Link href={`/post/${post.id}`}>Read more</Link>
+        </div>
+      ))}
     </div>
   );
 }

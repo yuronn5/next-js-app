869925881b674ef4bed422cd4383ec53f39bd5ec
yurii bilous <yurii.b@elogic.com.ua>
2024-10-19 19:31:50 +0200

    db working

--- app/api/data/route.js
+++ app/api/data/route.js
@@ -1,3 +1,15 @@
 export async function GET(request) {
-    return new Response('here is data');
+    const data = {
+        id: 1,
+        name: 'John Doe',   
+        age: 30
+    };
+    
+    return new Response(JSON.stringify(data) );
+}
+
+export async function POST(request) {
+    
+    
+    return new Response(JSON.stringify(data) );
 }

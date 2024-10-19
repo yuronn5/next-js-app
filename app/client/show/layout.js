d819fa71132e75fd471f7782d904a8ae13a7fdc5
yurii bilous <yurii.b@elogic.com.ua>
2024-10-19 10:49:51 +0200

    added styles for specific page (not only styles). we use layout.js

---
+++ app/client/show/layout.js
@@ -0,0 +1,26 @@
+import localFont from "next/font/local";
+import "./styles.css";
+
+
+const geistSans = localFont({
+  src: "../../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+    src: "../../fonts/GeistVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}

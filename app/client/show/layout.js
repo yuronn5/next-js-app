baaafe68f4fb8410bcf82e5e467dce1b67cf3e08
yurii bilous <yurii.b@elogic.com.ua>
2024-10-19 10:50:41 +0200

    added new heading only for that page

--- app/client/show/layout.js
+++ app/client/show/layout.js
@@ -19,6 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <h1>Top info</h1>
         {children}
       </body>
     </html>
